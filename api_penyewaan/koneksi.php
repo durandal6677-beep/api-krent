07ce8a7cf8f86c7cf42f178e2109bd5377e8d20f
Christian <durandal6677@gmail.com>
2026-05-29 14:12:32 +0700

    Update koneksi.php

--- api_penyewaan/koneksi.php
+++ api_penyewaan/koneksi.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = "QerLGuWHBhIQUmdenSbljbmekEwRjriV";
 $db   = "railway";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect("trolley.proxy.rlwy.net", "root", "QerLGuWHBhIQUmdenSbljbmekEwRjriV", "railway", 39090);
 
 if (!$conn) {
     echo json_encode(["status" => "error", "message" => "Koneksi Gagal"]);
